883bc29ff99bafe5ab82054647dd0a08fb2d7052
Ledia <ledialuli@icloud.com>
2024-02-29 14:54:23 +0000

    Simple Server CICD

---
+++ simple_server/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(
+      debug=True,
+      host="0.0.0.0" # Listen for connections _to_ any server
+    )
